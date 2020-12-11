a5b88a09fd20f93f9a8ca3026d3381f269fc45e4
locpham26 <loc.pham@tcu.edu>
2020-12-11 14:56:48 +0700

    create the video streaming server

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const mongoose = require('../database')
+const Schema = mongoose.Schema
+
+const videoSchema = new Schema({
+  title: {
+    type: String, required: true
+  },
+  author: {
+    type: String, required: true
+  },
+  url: {
+    type: String, required: true
+  },
+  view: {
+    type: Number, default: 0,
+  },
+  length: {
+    type: Number, default: 0,
+  },
+  deletedAt: {
+    type: Date, required: false
+  },
+}, {timestamps: true})
+
+const collectionName = 'video'
+
+const Video = mongoose.model(collectionName, videoSchema, collectionName)
+
+module.exports = {
+  Video,
+}
